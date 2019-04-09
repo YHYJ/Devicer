d6ecfe5b8943ec3f66bbf65435b7cdbf94076ee3
YJ <YJ1516268@outlook.com>
2019-04-09 08:12:38 +0800

    1.0a1 -> 1.0a2

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Author: YJ
-Email: yj1516268@outlook.com
-Created Time: 2019-03-29 09:37:36
-
-
-"""
-
 from setuptools import find_packages, setup
 
 setup(
     name='devicer',
-    version='1.0a1',
-    description='Device activity monitor',
+    version='1.0a2',
+    description='Linux device activity monitor',
     author='YJ',
     author_email='yj1516268@outlook.com',
-    url='https://github.com/YHYJ/Input-device-monitor',
+    url='https://github.com/YHYJ/Device-activity-monitor',
     license='MIT',
     packages=find_packages(exclude=[]),
     include_package_data=True,
